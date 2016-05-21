9fd0d5181160449e3628adda5a76c85ada3fc015
tsuyoshiwada <v23.b.wada@gmail.com>
2016-05-22 02:28:44 +0900

    clean up

--- counter/src/entry.js
+++ counter/src/entry.js
@@ -4,7 +4,6 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import App from "./containers/app";
 import configureStore from "./store/configureStore";
-import rootSaga from "./sagas/";
 
 const store = configureStore();
 
